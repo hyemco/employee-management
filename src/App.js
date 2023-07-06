23f146350237a2bd4af89eadf23ac1d6ef7aa38f
hyemco <hyemco@gmail.com>
2023-07-07 06:45:05 +0900

    Feat: get data from json-server
    Employee List 테이블 구조화
    json-server로 샘플 데이터 넣기
    데이터 가져와서 화면에 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmpListing from "./EmpListing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React JS CRUD Opertations</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EmpListing />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
